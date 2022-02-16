38e1e108a3a78e76ff9062eaef7eb8cdfb748dab
Adomasgra <79092790+Adomasgra@users.noreply.github.com>
2022-02-17 00:32:43 +0200

    camera movements
    Added camera movement

--- Assets/Scripts/Controls/CameraLookController.cs
+++ Assets/Scripts/Controls/CameraLookController.cs
@@ -9,13 +9,30 @@ public class CameraLookController : MonoBehaviour
     [SerializeField] private Transform lookTarget;
     [SerializeField] private PlayerMovementController movement;
 
+    public float mouseSensitivity = 15f;
+    
+    float x = 0f;
+    float y = 0f;
+
+    //float xRotation = 0f;
+
     void Update()
     {
         HandleRotationAroundCharacter();
+        
     }
 
     private void HandleRotationAroundCharacter() //TODO
     {
-        throw new NotImplementedException();
+        x += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
+        y += Input.GetAxis("Mouse Y") * -1 * mouseSensitivity * 0.1f;
+
+        //xRotation -= y;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //lookTarget.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        //lookTarget.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        lookTarget.transform.localEulerAngles = new Vector3(y,x,0);
     }
 }
