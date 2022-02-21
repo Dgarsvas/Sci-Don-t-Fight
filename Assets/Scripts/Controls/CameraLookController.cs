b43f2620df04c0b6d8f6477db1c91807857aea08
DomantasGarsvas <dgarsvas@gmail.com>
2022-02-21 19:32:48 +0200

    Added auto look snaping, implemented physics based movement

--- Assets/Scripts/Controls/CameraLookController.cs
+++ Assets/Scripts/Controls/CameraLookController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CameraLookController : MonoBehaviour
 {
     [SerializeField] private Transform lookTarget;
+
     [SerializeField] private Transform playerTransform;
     [SerializeField] private PlayerMovementController movement;
 
@@ -14,21 +15,65 @@ public class CameraLookController : MonoBehaviour
     [SerializeField] float mouseSensitivity = 15f;
     [SerializeField] float maxAngle = 85f;
 
-    private float x = 0f;
-    private float y = 0f;
+    private float horizontal = 0f;
+    private float vertical = 0f;
+    private float bodyHorizontal = 0f;
+
+    private const float RETURN_SPEED = 5f;
 
     void Update()
     {
         HandleRotationAroundCharacter();
-
     }
 
-    private void HandleRotationAroundCharacter() //TODO
+    private void HandleRotationAroundCharacter()
     {
-        x += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
-        y += Input.GetAxis("Mouse Y") * -1 * mouseSensitivity * 0.1f;
+        vertical += Input.GetAxis("Mouse Y") * -1 * mouseSensitivity * 0.1f;
 
-        lookTarget.transform.localEulerAngles = new Vector3(Mathf.Clamp(y, -maxAngle, maxAngle), x, 0);
-        lookTarget.transform.position = playerTransform.position;
+        if (movement.IsMoving && !Input.GetKey(KeyCode.LeftAlt))
+        {
+            bodyHorizontal += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
+            horizontal = MoveBackToDefaultPos(horizontal);
+            playerTransform.transform.transform.localEulerAngles = new Vector3(playerTransform.transform.transform.localEulerAngles.x, bodyHorizontal);
+            lookTarget.transform.localEulerAngles = new Vector3(Mathf.Clamp(vertical, -maxAngle, maxAngle), horizontal, 0);
+        }
+        else
+        {
+            horizontal += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
+            lookTarget.transform.localEulerAngles = new Vector3(Mathf.Clamp(vertical, -maxAngle, maxAngle), horizontal, 0);
+        }
+    }
+
+    private float MoveBackToDefaultPos(float y)
+    {
+        if (Mathf.Abs(y) > Mathf.Epsilon)
+        {
+            if (y > 0)
+            {
+                if (y - RETURN_SPEED < 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return y - RETURN_SPEED;
+                }
+            }
+            else
+            {
+                if (y + RETURN_SPEED > 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return y + RETURN_SPEED;
+                }
+            }
+        }
+        else
+        {
+            return 0f;
+        }
     }
 }
--- Assets/Scripts/Controls/PlayerMovementController.cs
+++ Assets/Scripts/Controls/PlayerMovementController.cs
@@ -7,91 +7,64 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Collider col;
-    [SerializeField] private CameraLookController lookController;
+    [SerializeField] private Transform body;
     [SerializeField] private float overlapBoxOffset;
-    [SerializeField] private Transform lookTarget;
 
     [Header("Movement Settings")]
     [SerializeField] private float jumpForce;
     [SerializeField] private float walkSpeed;
-    [SerializeField] private float turnSmoothTime;
+    [SerializeField] private float maxSpeed;
 
-    // is this how you declare constants in C# scripts?
-    private int numCollidersInPlayerToIgnore;
+    private int numCollidersInPlayerToIgnore;    
 
-    private float turnSmoothVelocity;
-
-    public bool IsOnGround
+    private bool IsOnGround()
     {
-        get;
-        private set;
+        var colliders = Physics.OverlapBox(rb.position + Vector3.down * overlapBoxOffset, transform.localScale / 2);
+        return colliders.Length > 1;
     }
 
-    public bool IsMoving 
-    {
-        get;
-        private set;
-    }
+    public bool IsMoving => rb.velocity.magnitude > 0.0f;
 
     private void Start()
     {
         numCollidersInPlayerToIgnore = GetComponentsInChildren<Collider>().Length;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         HandleJump();
 
-        if (IsOnGround)
+        if (IsOnGround())
         {
             HandleMovementAndRotation();
         }
     }
 
-    // Debug ground check
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(rb.position + Vector3.down * overlapBoxOffset, transform.localScale);
     }
 
-    private void FixedUpdate()
-    {
-        // transform is of Player object and doesn't reflect the scale of player mesh. Should we change this to something else?
-        var colliders = Physics.OverlapBox(rb.position + Vector3.down * overlapBoxOffset, transform.localScale / 2);
-        IsOnGround = colliders.Length > 1;
-
-        IsMoving = rb.velocity.magnitude > 0.0f;
-    }
-
     private void HandleMovementAndRotation()
     {
-        float sideMovement = Input.GetAxisRaw("Horizontal") * walkSpeed;
-        float fwdMovement = Input.GetAxisRaw("Vertical") * walkSpeed;
+        Vector3 sideMovement = body.right * Input.GetAxisRaw("Horizontal");
+        Vector3 fwdMovement = body.forward * Input.GetAxisRaw("Vertical");
 
-        // If i use rigidbody here then it starts jittering. Probably because physics updates happen every fixed update or something like that.
-        // Should I update the position of transform or RB?
-        var direction = new Vector3(sideMovement, 0.0f, fwdMovement).normalized;
+        var direction = (sideMovement + fwdMovement) * walkSpeed;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + lookTarget.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(rb.rotation.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            rb.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-
-            Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-            // Problem with this is that this gives no velocity
-            // e.g. when you jump while moving you will jump straight up
-            rb.MovePosition(rb.position + moveDir.normalized * walkSpeed * Time.deltaTime);
+            rb.velocity = new Vector3(direction.x, rb.velocity.y, direction.z);
         }
     }
 
     private void HandleJump() 
     {
-        if (IsOnGround && Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
         {
-            rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f));
-            IsOnGround = false;
+            rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.VelocityChange);
+            //IsOnGround = false;
         }
     }
 }
