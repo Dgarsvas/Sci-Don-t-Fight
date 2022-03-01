c55baa6b26364df22f96c0a52de1502150430dc0
DomantasGarsvas <dgarsvas@gmail.com>
2022-03-01 19:30:38 +0200

    Made the movement and look controller be much more pleasant

--- Assets/Scripts/Controls/CameraLookController.cs
+++ Assets/Scripts/Controls/CameraLookController.cs
@@ -8,7 +8,7 @@ public class CameraLookController : MonoBehaviour
 {
     [SerializeField] private Transform lookTarget;
 
-    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private PlayerMovementController movement;
 
     [Header("Look Settings")]
@@ -21,27 +21,32 @@ public class CameraLookController : MonoBehaviour
 
     private const float RETURN_SPEED = 5f;
 
-    void Update()
+    void FixedUpdate()
     {
         HandleRotationAroundCharacter();
     }
 
     private void HandleRotationAroundCharacter()
     {
-        vertical += Input.GetAxis("Mouse Y") * -1 * mouseSensitivity * 0.1f;
+        vertical = Mathf.Clamp(vertical + Input.GetAxis("Mouse Y") * -1 * mouseSensitivity * Time.fixedDeltaTime, -maxAngle, maxAngle);
+        bodyHorizontal += Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
 
+        Vector3 prevRot = rb.rotation.eulerAngles;
+        rb.rotation = Quaternion.Euler(new Vector3(prevRot.x, bodyHorizontal, prevRot.z));
+        lookTarget.transform.localEulerAngles = new Vector3(vertical, 0f, 0f);
+        /*
         if (movement.IsMoving && !Input.GetKey(KeyCode.LeftAlt))
         {
             bodyHorizontal += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
-            horizontal = MoveBackToDefaultPos(horizontal);
             playerTransform.transform.transform.localEulerAngles = new Vector3(playerTransform.transform.transform.localEulerAngles.x, bodyHorizontal);
+            horizontal = MoveBackToDefaultPos(horizontal);
             lookTarget.transform.localEulerAngles = new Vector3(Mathf.Clamp(vertical, -maxAngle, maxAngle), horizontal, 0);
         }
         else
         {
             horizontal += Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
             lookTarget.transform.localEulerAngles = new Vector3(Mathf.Clamp(vertical, -maxAngle, maxAngle), horizontal, 0);
-        }
+        }*/
     }
 
     private float MoveBackToDefaultPos(float y)
--- Assets/Scripts/Controls/PlayerMovementController.cs
+++ Assets/Scripts/Controls/PlayerMovementController.cs
@@ -6,47 +6,77 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private Collider col;
     [SerializeField] private Transform body;
-    [SerializeField] private float overlapBoxOffset;
 
     [Header("Movement Settings")]
     [SerializeField] private float jumpForce;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private Transform feetTransform;
 
-    private int numCollidersInPlayerToIgnore;    
+
+    [Header("Stepping Settings")]
+    [SerializeField] private GameObject stepRayUpper;
+    [SerializeField] private GameObject stepRayLower;
+    [SerializeField] private float stepHeight = 0.3f;
+    [SerializeField] private float stepSmooth = 0.1f;
 
     private bool IsOnGround()
     {
-        var colliders = Physics.OverlapBox(rb.position + Vector3.down * overlapBoxOffset, transform.localScale / 2);
-        return colliders.Length > 1;
+        return Physics.CheckSphere(feetTransform.position, 0.15f, groundLayers);
     }
 
     public bool IsMoving => rb.velocity.magnitude > 0.0f;
 
-    private void Start()
+    private void Awake()
     {
-        numCollidersInPlayerToIgnore = GetComponentsInChildren<Collider>().Length;
+        stepRayUpper.transform.position = stepRayLower.transform.position + new Vector3(0f, stepHeight, 0f);
     }
 
     void FixedUpdate()
     {
-        HandleJump();
-
-        if (IsOnGround())
+        if (Input.GetKey(KeyCode.Space))
         {
-            HandleMovementAndRotation();
+            if (IsOnGround())
+            {
+                rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.Impulse);
+            }
+        }
+
+        HandleMovement();
+        HandleSteps();
+    }
+
+    private void HandleSteps()
+    {
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitLower, 0.6f, groundLayers))
+        {
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitUpper, 0.6f, groundLayers))
+            {
+                rb.position += new Vector3(0f, stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out RaycastHit hitLower45, 0.6f, groundLayers))
+        {
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out RaycastHit hitUpper45, 0.6f))
+            {
+                rb.position += new Vector3(0f, stepSmooth * Time.deltaTime, 0f);
+            }
+        }
+
+        if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out RaycastHit hitLowerMinus45, 0.6f, groundLayers))
+        {
+
+            if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out RaycastHit hitUpperMinus45, 0.6f, groundLayers))
+            {
+                rb.position += new Vector3(0f, stepSmooth * Time.deltaTime, 0f);
+            }
         }
     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(rb.position + Vector3.down * overlapBoxOffset, transform.localScale);
-    }
-
-    private void HandleMovementAndRotation()
+    private void HandleMovement()
     {
         Vector3 sideMovement = body.right * Input.GetAxisRaw("Horizontal");
         Vector3 fwdMovement = body.forward * Input.GetAxisRaw("Vertical");
@@ -57,14 +87,9 @@ public class PlayerMovementController : MonoBehaviour
         {
             rb.velocity = new Vector3(direction.x, rb.velocity.y, direction.z);
         }
-    }
-
-    private void HandleJump() 
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
+        else
         {
-            rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.VelocityChange);
-            //IsOnGround = false;
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
         }
     }
 }
