b330b931119e577893e4ea8fb573e73530258997
matas.tunkevicius <matas.tunkevicius@unity3d.com>
2022-05-11 12:39:21 +0300

    Added freezegun pickup placeholder in scene

---
+++ Assets/Scripts/Interactable/InteractableSupply.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableSupply : InteractableBase
+{
+    [SerializeField] private UnityEvent onButtonPress;
+    [SerializeField] private Renderer rend;
+    private Material mat;
+
+    private void Awake()
+    {
+        mat = rend.material;
+    }
+
+    public override void Highlight()
+    {
+        IsHighlighted = true;
+        rend.material = GameManager.Instance.highlightMat;
+    }
+
+    internal override void StopHighlight()
+    {
+        IsHighlighted = false;
+        rend.material = mat;
+    }
+
+    public override void InteractionContinue()
+    {
+    }
+
+    public override void InteractionEnd()
+    {
+    }
+
+    public override void InteractionStart()
+    {
+        Debug.Log("Interaction Start");
+        onButtonPress?.Invoke();
+    }
+}
